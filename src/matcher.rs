28145c70f5e1f6a3dabc69d3e4ab3e208103f28f
electricboogie <32370782+electricboogie@users.noreply.github.com>
2023-03-18 18:51:45 -0500

    Revert "Avoid indirection"
    This reverts commit 301ce1d81ce0b649df46a30c0cc6afb3b7a44380.

--- src/matcher.rs
+++ src/matcher.rs
@@ -105,12 +105,23 @@ impl Matcher {
 
             trace!("matcher start, total: {}", items.len());
 
-            let filter_op = |index: usize, item: &Arc<dyn SkimItem>| -> Option<MatchedItem> {
+            let filter_op = |index: usize, item: &Arc<dyn SkimItem>| -> Option<Result<MatchedItem, &str>> {
                 processed.fetch_add(1, Ordering::Relaxed);
+
+                if matcher_disabled {
+                    return Some(Ok(MatchedItem {
+                        item: Arc::downgrade(item),
+                        metadata: None,
+                    }));
+                }
+
+                if stopped.load(Ordering::Relaxed) {
+                    return Some(Err("matcher killed"));
+                }
 
                 matcher_engine.match_item(item.as_ref()).map(|match_result| {
                     matched.fetch_add(1, Ordering::Relaxed);
-                    MatchedItem {
+                    Ok(MatchedItem {
                         item: Arc::downgrade(item),
                         metadata: {
                             Some(Box::new({
@@ -121,31 +132,17 @@ impl Matcher {
                                 }
                             }))
                         },
-                    }
+                    })
                 })
             };
 
-            let result: Result<Vec<_>, _> = if matcher_disabled {
-                Ok(MATCHER_POOL.install(|| {
-                    items
-                        .iter()
-                        .map(|item| MatchedItem {
-                            item: Arc::downgrade(item),
-                            metadata: None,
-                        })
-                        .collect()
-                }))
-            } else if stopped.load(Ordering::Relaxed) {
-                Err("matcher killed")
-            } else {
-                Ok(MATCHER_POOL.install(|| {
-                    items
-                        .par_iter()
-                        .enumerate()
-                        .filter_map(|(index, item)| filter_op(index, item))
-                        .collect()
-                }))
-            };
+            let result: Result<Vec<_>, _> = MATCHER_POOL.install(|| {
+                items
+                    .par_iter()
+                    .enumerate()
+                    .filter_map(|(index, item)| filter_op(index, item))
+                    .collect()
+            });
 
             if let Ok(items) = result {
                 let mut pool = matched_items.lock();
