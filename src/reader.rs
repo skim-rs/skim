66dacfae634ae88e84e9b64ba4819485e5abb832
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2023-05-19 19:42:22 -0500

    Increase read lines to 50

--- src/reader.rs
+++ src/reader.rs
@@ -149,7 +149,7 @@ fn collect_item(
                             break
                         }
 
-                       locked.extend(rx_item.iter().take(20))
+                       locked.extend(rx_item.iter().take(50))
                     },
                     i if i == interrupt_channel => break,
                     _ => unreachable!(),
