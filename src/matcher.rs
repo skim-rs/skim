457dc6b6967eab2038f705271cff03bfe41c823d
electricboogie <32370782+kimono-koans@users.noreply.github.com>
2024-02-22 19:19:51 -0600

    Cleanup

--- src/matcher.rs
+++ src/matcher.rs
@@ -198,9 +198,7 @@ fn process_item(
     item: &Arc<dyn SkimItem>,
 ) -> Option<MatchedItem> {
     matcher_engine.match_item(item.as_ref()).map(|match_result| {
-        if index % 256 == 0 {
-            matched.fetch_add(256, Ordering::Relaxed);
-        }
+        matched.fetch_add(256, Ordering::Relaxed);
 
         MatchedItem {
             item: Arc::downgrade(item),
