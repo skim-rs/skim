24303747810501ce976d7bed190e7141fc4db593
LoricAndre <loric.andre@pm.me>
2024-11-30 17:25:39 +0100

    check len

--- e2e/tests/issues.rs
+++ e2e/tests/issues.rs
@@ -8,7 +8,7 @@ fn issue_359_multi_regex_unicode() -> Result<()> {
     tmux.start_sk(Some("echo 'ああa'"), &["--regex", "-q", "'a'"])?;
     tmux.until(|l| l[0] == "> a")?;
 
-    tmux.until(|l| l[2] == "> ああa")?;
+    tmux.until(|l| l.len() > 2 && l[2] == "> ああa")?;
 
     Ok(())
 }
@@ -18,7 +18,7 @@ fn issue_361_literal_space_control() -> Result<()> {
     let tmux = TmuxController::new()?;
     tmux.start_sk(Some("echo -ne 'foo bar\\nfoo  bar'"), &["-q", "'foo\\ bar'"])?;
     tmux.until(|l| l[0].starts_with(">"))?;
-    tmux.until(|l| l[2] == "> foo bar")?;
+    tmux.until(|l| l.len() > 2 && l[2] == "> foo bar")?;
 
     Ok(())
 }
@@ -28,7 +28,7 @@ fn issue_361_literal_space_invert() -> Result<()> {
     tmux.send_keys(&[Str("set +o histexpand"), Enter])?;
     tmux.start_sk(Some("echo -ne 'foo bar\\nfoo  bar'"), &["-q", "'!foo\\ bar'"])?;
     tmux.until(|l| l[0].starts_with(">"))?;
-    tmux.until(|l| l[2] == "> foo  bar")?;
+    tmux.until(|l| l.len() > 2 && l[2] == "> foo  bar")?;
 
     Ok(())
 }
