ed1c48d6d0e232ffe4e63e3294cd41664dfdf654
LoricAndre <57358788+LoricAndre@users.noreply.github.com>
2025-04-26 12:34:42 +0200

    fix(filter): fix broken pipe while writing results to locked stdout (closes #733) (#737)
    Co-authored-by: LoricAndre <loric.andre@pm.me>

--- skim/src/bin/main.rs
+++ skim/src/bin/main.rs
@@ -229,12 +229,13 @@ pub fn filter(bin_option: &BinOptions, options: &SkimOptions, source: Option<Ski
     });
 
     let mut num_matched = 0;
+    let mut stdout_lock = std::io::stdout().lock();
     stream_of_item
         .into_iter()
         .filter_map(|item| engine.match_item(item.clone()).map(|result| (item, result)))
         .for_each(|(item, _match_result)| {
             num_matched += 1;
-            print!("{}{}", item.output(), bin_option.output_ending);
+            let _ = write!(stdout_lock, "{}{}", item.output(), bin_option.output_ending);
         });
 
     i32::from(num_matched == 0)
