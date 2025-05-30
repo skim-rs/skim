753a7600d43b16ed0527a2ba18fd3182610b1af9
LoricAndre <loric.andre@pm.me>
2025-05-30 12:51:43 +0200

    Fix formatting issues

--- skim/src/bin/main.rs
+++ skim/src/bin/main.rs
@@ -82,11 +82,11 @@ fn sk_main() -> Result<i32, SkMainError> {
     // Handle shell completion generation if requested
     if let Some(shell) = opts.shell {
         // Use the Shell enum directly
-        
+
         // Generate completion script directly to stdout
         generate(shell, &mut SkimOptions::command(), "sk", &mut io::stdout());
         return Ok(0);
-        }
+    }
 
     let reader_opts = SkimItemReaderOption::default()
         .ansi(opts.ansi)
