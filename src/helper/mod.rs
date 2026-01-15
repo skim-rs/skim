21586f911cfb388dd5d68f2247a23ae309b0fb31
Loric ANDRE <loric.andre@pm.me>
2026-01-15 11:41:20 +0100

    chore: add exhaustive_match macro for enum building from str

--- src/helper/mod.rs
+++ src/helper/mod.rs
@@ -1,3 +1,5 @@
 pub mod item;
 pub mod item_reader;
+#[macro_use]
+pub mod macros;
 pub mod selector;
--- src/tui/event.rs
+++ src/tui/event.rs
@@ -1,3 +1,4 @@
+use crate::exhaustive_match;
 use crossterm::event::{KeyEvent, MouseEvent};
 
 /// Events that can occur during skim's execution
@@ -173,108 +174,123 @@ pub enum Action {
 }
 
 /// Parses an action string into an Action enum
-#[rustfmt::skip]
 pub fn parse_action(raw_action: &str) -> Option<Action> {
-  let parts = raw_action.split_once([':', '(', ')']);
-  let action;
-  let mut arg = None;
-  match parts {
-    None => { action = raw_action }
-    Some((act, "")) => { action = act }
-    Some((act, a)) => { action = act; arg = Some(a.trim_end_matches(")").to_string()) }
-  }
-  debug!("parse_action: action={action}, arg={arg:?}");
-
-  // Parse `if` chains
-  if action.starts_with("if-") {
-    let then_arg;
-    let mut otherwise_arg = None;
-
-    let if_arg = arg.unwrap_or_else(|| panic!("no arg specified for event {action}"));
-    if if_arg.contains("+") {
-      let split = if_arg.split_once("+");
-      match split {
-        Some((a, "")) => { then_arg = a.to_string(); }
-        Some((a, b)) => {
-          then_arg = a.to_string();
-          otherwise_arg = Some(b.to_string());
+    let parts = raw_action.split_once([':', '(', ')']);
+    let action;
+    let mut arg = None;
+    match parts {
+        None => action = raw_action,
+        Some((act, "")) => action = act,
+        Some((act, a)) => {
+            action = act;
+            arg = Some(a.trim_end_matches(")").to_string())
         }
-        None => unreachable!()
-      }
-    } else {
-      then_arg = if_arg.to_string();
     }
-    match action {
-      "if-non-matched"       =>   Some(Action::IfNonMatched(then_arg, otherwise_arg)),
-      "if-query-empty"       =>   Some(Action::IfQueryEmpty(then_arg, otherwise_arg)),
-      "if-query-not-empty"   =>   Some(Action::IfQueryNotEmpty(then_arg, otherwise_arg)),
-      _ => None
-    }
-  } else {
-    use Action::*;
-    match action {
-          "abort"                    =>   Some(Abort),
-          "accept"                   =>   Some(Accept(arg)),
-          "add-char"                 => Some(AddChar(arg.unwrap_or_default().chars().next().expect("add-char should have an argument"))),
-          "append-and-select"        =>   Some(AppendAndSelect),
-          "backward-char"            =>   Some(BackwardChar),
-          "backward-delete-char"     =>   Some(BackwardDeleteChar),
-          "backward-delete-char/eof" =>   Some(BackwardDeleteCharEof),
-          "backward-kill-word"       =>   Some(BackwardKillWord),
-          "backward-word"            =>   Some(BackwardWord),
-          "beginning-of-line"        =>   Some(BeginningOfLine),
-          "cancel"                   =>   Some(Cancel),
-          "clear-screen"             =>   Some(ClearScreen),
-          "delete-char"              =>   Some(DeleteChar),
-          "delete-char/eof"          =>   Some(DeleteCharEof),
-          "deselect-all"             =>   Some(DeselectAll),
-          "down"                     =>   Some(Down(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "end-of-line"              =>   Some(EndOfLine),
-          "execute"                  =>   Some(Execute(arg.expect("execute event should have argument"))),
-          "execute-silent"           =>   Some(ExecuteSilent(arg.expect("execute-silent event should have argument"))),
-          "first"                    =>   Some(First),
-          "forward-char"             =>   Some(ForwardChar),
-          "forward-word"             =>   Some(ForwardWord),
-          "ignore"                   =>   Some(Ignore),
-          "kill-line"                =>   Some(KillLine),
-          "kill-word"                =>   Some(KillWord),
-          "last"                     =>   Some(Last),
-          "next-history"             =>   Some(NextHistory),
-          "half-page-down"           =>   Some(HalfPageDown(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "half-page-up"             =>   Some(HalfPageUp(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "page-down"                =>   Some(PageDown(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "page-up"                  =>   Some(PageUp(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "preview-up"               =>   Some(PreviewUp(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "preview-down"             =>   Some(PreviewDown(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "preview-left"             =>   Some(PreviewLeft(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "preview-right"            =>   Some(PreviewRight(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "preview-page-up"          =>   Some(PreviewPageUp(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "preview-page-down"        =>   Some(PreviewPageDown(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "previous-history"         =>   Some(PreviousHistory),
-          "redraw"                   => Some(Redraw),
-          "refresh-cmd"              =>   Some(RefreshCmd),
-          "refresh-preview"          =>   Some(RefreshPreview),
-          "reload"                   =>   Some(Reload(arg.clone())),
-          "scroll-left"              =>   Some(ScrollLeft(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "scroll-right"             =>   Some(ScrollRight(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "select"                   => Some(Select),
-          "select-all"               =>   Some(SelectAll),
-          "select-row"               => Some(SelectRow(arg.and_then(|s| s.parse().ok()).unwrap_or_default())),
-          "toggle"                   =>   Some(Toggle),
-          "toggle-all"               =>   Some(ToggleAll),
-          "toggle-in"                =>   Some(ToggleIn),
-          "toggle-interactive"       =>   Some(ToggleInteractive),
-          "toggle-out"               =>   Some(ToggleOut),
-          "toggle-preview"           =>   Some(TogglePreview),
-          "toggle-preview-wrap"      =>   Some(TogglePreviewWrap),
-          "toggle-sort"              =>   Some(ToggleSort),
-          "top"                      =>   Some(Top),
-          "unix-line-discard"        =>   Some(UnixLineDiscard),
-          "unix-word-rubout"         =>   Some(UnixWordRubout),
-          "up"                       =>   Some(Up(arg.and_then(|s|s.parse().ok()).unwrap_or(1))),
-          "yank"                     =>   Some(Yank),
-          _ => None
-  }
+    debug!("parse_action: action={action}, arg={arg:?}");
 
+    // Parse `if` chains
+    if action.starts_with("if-") {
+        let then_arg;
+        let mut otherwise_arg = None;
+
+        let if_arg = arg.unwrap_or_else(|| panic!("no arg specified for event {action}"));
+        if if_arg.contains("+") {
+            let split = if_arg.split_once("+");
+            match split {
+                Some((a, "")) => {
+                    then_arg = a.to_string();
+                }
+                Some((a, b)) => {
+                    then_arg = a.to_string();
+                    otherwise_arg = Some(b.to_string());
+                }
+                None => unreachable!(),
+            }
+        } else {
+            then_arg = if_arg.to_string();
+        }
+        match action {
+            "if-non-matched" => Some(Action::IfNonMatched(then_arg, otherwise_arg)),
+            "if-query-empty" => Some(Action::IfQueryEmpty(then_arg, otherwise_arg)),
+            "if-query-not-empty" => Some(Action::IfQueryNotEmpty(then_arg, otherwise_arg)),
+            _ => None,
+        }
+    } else {
+        exhaustive_match! {
+            action => Option<Action>;
+            {
+                "abort" => Some(Abort),
+                "accept" => Some(Accept(arg)),
+                "add-char" => Some(AddChar(
+                    arg.unwrap_or_default()
+                        .chars()
+                        .next()
+                        .expect("add-char should have an argument"),
+                )),
+                "append-and-select" => Some(AppendAndSelect),
+                "backward-char" => Some(BackwardChar),
+                "backward-delete-char" => Some(BackwardDeleteChar),
+                "backward-delete-char/eof" => Some(BackwardDeleteCharEof),
+                "backward-kill-word" => Some(BackwardKillWord),
+                "backward-word" => Some(BackwardWord),
+                "beginning-of-line" => Some(BeginningOfLine),
+                "cancel" => Some(Cancel),
+                "clear-screen" => Some(ClearScreen),
+                "delete-char" => Some(DeleteChar),
+                "delete-char/eof" => Some(DeleteCharEof),
+                "deselect-all" => Some(DeselectAll),
+                "down" => Some(Down(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "end-of-line" => Some(EndOfLine),
+                "execute" => Some(Execute(arg.expect("execute event should have argument"))),
+                "execute-silent" => Some(ExecuteSilent(arg.expect("execute-silent event should have argument"))),
+                "first" => Some(First),
+                "forward-char" => Some(ForwardChar),
+                "forward-word" => Some(ForwardWord),
+                "ignore" => Some(Ignore),
+                "kill-line" => Some(KillLine),
+                "kill-word" => Some(KillWord),
+                "last" => Some(Last),
+                "next-history" => Some(NextHistory),
+                "half-page-down" => Some(HalfPageDown(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "half-page-up" => Some(HalfPageUp(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "page-down" => Some(PageDown(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "page-up" => Some(PageUp(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "preview-up" => Some(PreviewUp(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "preview-down" => Some(PreviewDown(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "preview-left" => Some(PreviewLeft(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "preview-right" => Some(PreviewRight(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "preview-page-up" => Some(PreviewPageUp(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "preview-page-down" => Some(PreviewPageDown(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "previous-history" => Some(PreviousHistory),
+                "redraw" => Some(Redraw),
+                "refresh-cmd" => Some(RefreshCmd),
+                "refresh-preview" => Some(RefreshPreview),
+                "restart-matcher" => Some(RestartMatcher),
+                "reload" => Some(Reload(arg.clone())),
+                "rotate-mode" => Some(RotateMode),
+                "scroll-left" => Some(ScrollLeft(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "scroll-right" => Some(ScrollRight(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "select" => Some(Select),
+                "select-all" => Some(SelectAll),
+                "select-row" => Some(SelectRow(arg.and_then(|s| s.parse().ok()).unwrap_or_default())),
+                "toggle" => Some(Toggle),
+                "toggle-all" => Some(ToggleAll),
+                "toggle-in" => Some(ToggleIn),
+                "toggle-interactive" => Some(ToggleInteractive),
+                "toggle-out" => Some(ToggleOut),
+                "toggle-preview" => Some(TogglePreview),
+                "toggle-preview-wrap" => Some(TogglePreviewWrap),
+                "toggle-sort" => Some(ToggleSort),
+                "top" => Some(Top),
+                "unix-line-discard" => Some(UnixLineDiscard),
+                "unix-word-rubout" => Some(UnixWordRubout),
+                "up" => Some(Up(arg.and_then(|s| s.parse().ok()).unwrap_or(1))),
+                "yank" => Some(Yank),
+                "unreachable-if-non-matched" => Some(IfNonMatched(Default::default(), None)),
+                "unreachable-if-query-empty" => Some(IfQueryEmpty(Default::default(), None)),
+                "unreachable-if-query-not-empty" => Some(IfQueryNotEmpty(Default::default(), None))
+            }
+            default _ => None
+        }
     }
 }
